32d986f8ef4a38055bf2f3a53e792bdd713c8201
Benjamin Riggs <deisum@gmail.com>
2020-06-01 19:37:58 -0500

    Work with Windows line endings

--- compare_sdk_config.sh
+++ compare_sdk_config.sh
@@ -2,7 +2,7 @@
 
 if [[ $# < 2 ]]
 then
-echo "Script takes 2+ arguments, a project Sdk_config.h and one or more example sdk_config.h, \
+echo "Script takes 2+ arguments, a project sdk_config.h and one or more example sdk_config.h, \
 and displays settings in each example sdk_config.h that differ or are missing from project sdk_config.h"
 exit
 fi
@@ -10,7 +10,7 @@ fi
 PROJECT_CONFIG=$1
 
 compare() {
-    local IFS=$'\n'
+    local IFS=$'\n\r'
     for EXAMPLE_OPTION in $( grep "#define" "$1" )
     do
         OPTION_NAME=$(printf "${EXAMPLE_OPTION}" | cut -d' ' -f2)
