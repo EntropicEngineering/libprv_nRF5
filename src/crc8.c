0a298ef723e6f3dcd089232014ee4ceb1669789c
Benjamin Riggs <deisum@gmail.com>
2020-11-04 21:45:49 -0600

    Add missing crc8 features

--- src/crc8.c
+++ src/crc8.c
@@ -4,13 +4,25 @@
 
 #include "crc8.h"
 
-void crc8_init(crc8_t *p_crc8, uint8_t polynomial, uint8_t initial_val, uint8_t final_val) {
-    p_crc8->initial_value = initial_val;
-    p_crc8->final_value = final_val;
+static __inline uint8_t bit_reverse (uint8_t b) {
+    uint32_t res;
+    // RBIT reverses all bits in a 32-bit register
+    __asm("RBIT %[out], %[in]"
+        : [out] "=r" (res)
+        : [in] "r" (b)
+        );
+    return (uint8_t) (res >> 24);
+}
+
+void crc8_init(crc8_t *p_crc8, crc8_conf_t conf) {
+    p_crc8->initial_value = conf.initial_value;
+    p_crc8->final_value = conf.final_value;
+    p_crc8->reflect_input = conf.reflect_input;
+    p_crc8->reflect_result = conf.reflect_result;
     for (size_t i = 0; i < 256; i++) {
         uint8_t crc = i;
         for (uint8_t bit = 0; bit < 8; bit++) {
-            crc = (crc & 0x80) ? ((crc << 1) ^ polynomial) : (crc << 1);
+            crc = (crc & 0x80) ? ((crc << 1) ^ conf.polynomial) : (crc << 1);
         }
         p_crc8->lookup_table[i] = crc;
     }
@@ -18,10 +30,18 @@ void crc8_init(crc8_t *p_crc8, uint8_t polynomial, uint8_t initial_val, uint8_t 
 
 uint8_t crc8_calc(crc8_t *p_crc8, uint8_t *p_data, size_t length) {
     uint8_t crc = p_crc8->initial_value;
-    while (length) {
-        crc = p_crc8->lookup_table[crc ^ *p_data];
-        p_data++;
-        length--;
+    if (p_crc8->reflect_input) {
+        while (length) {
+            crc = p_crc8->lookup_table[crc ^ bit_reverse(*p_data)];
+            p_data++;
+            length--;
+        }
+    } else {
+        while (length) {
+            crc = p_crc8->lookup_table[crc ^ *p_data];
+            p_data++;
+            length--;
+        }
     }
-    return p_crc8->final_value ^ crc;
+    return p_crc8->reflect_result ? bit_reverse(crc) ^ p_crc8->final_value : crc ^ p_crc8->final_value;
 }
--- src/crc8.h
+++ src/crc8.h
@@ -6,34 +6,51 @@
 #define CRC8_H
 
 #include <stdint.h>
+#include <stdbool.h>
 #include <stddef.h>
+
+/**@brief crc-8 calculator configuration.
+ */
+typedef struct {
+    uint8_t polynomial;
+    uint8_t initial_value;
+    uint8_t final_value;
+    bool reflect_input;
+    bool reflect_result;
+} crc8_conf_t;
+
+#define CRC8_CONF (crc8_conf_t) { \
+    .polynomial = 0x07, \
+    .initial_value = 0, \
+    .final_value = 0, \
+    .reflect_intput = false, \
+    .reflect_result = false \
+}
 
 typedef struct {
     uint8_t initial_value;
     uint8_t final_value;
+    bool reflect_input;
+    bool reflect_result;
     uint8_t lookup_table[256];
 } crc8_t;
-
-#define CRC8_INIT(p_crc8) crc8_init(p_crc8, 0x07, 0, 0)
 
 /**@brief   Initialize crc-8 calculator.
  *
  * This will generate a lookup table in RAM based on the polynomial, and fix the initial and final values used in
  * CRC calculations.
  *
- * @param p_crc8            Pointer to memory where crc8 structure values will be stored.
- * @param polynomial        Polynomial used by CRC
- * @param initial_value     Initialization value given to CRC calculation.
- * @param final_value       Final value XOR'd with result of CRC calculation.
+ * @param p_crc8            Pointer to memory where crc-8 structure values will be stored.
+ * @param crc8_conf         Configuration parameters for crc-8 calculator.
  */
-void crc8_init(crc8_t *p_crc8, uint8_t polynomial, uint8_t initial_value, uint8_t final_value);
+void crc8_init(crc8_t *p_crc8, crc8_conf_t crc8_conf);
 
 /**@brief   Calculate the crc-8 of a data.
  *
- * @param p_crc     Pointer to initialized crc8 structure.
+ * @param p_crc8    Pointer to initialized crc-8 structure.
  * @param buffer    Data to calculate CRC over.
  * @param length    Length of data.
- * @return          Calculated CRC value for data and given crc8.
+ * @return          Calculated CRC value for data and given crc-8.
  */
 uint8_t crc8_calc(crc8_t *p_crc8, uint8_t *p_data, size_t length);
 
