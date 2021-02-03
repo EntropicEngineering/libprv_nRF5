b1eb7a7407cf4bbefe5629eecdcf1e652a091e64
Benjamin Riggs <deisum@gmail.com>
2021-02-03 05:39:23 -0600

    Minor improvements.

--- src/crc8.c
+++ src/crc8.c
@@ -4,6 +4,8 @@
 
 #include "crc8.h"
 
+#include "nrfx.h"
+
 static __inline uint8_t bit_reverse (uint8_t b) {
     uint32_t res;
     // RBIT reverses all bits in a 32-bit register
@@ -15,6 +17,8 @@ static __inline uint8_t bit_reverse (uint8_t b) {
 }
 
 void crc8_init(crc8_t *p_crc8, crc8_conf_t const *p_crc8_conf) {
+    ASSERT(p_crc8);
+    ASSERT(p_crc8_conf);
     p_crc8->initial_value = p_crc8_conf->initial_value;
     p_crc8->final_value = p_crc8_conf->final_value;
     p_crc8->reflect_input = p_crc8_conf->reflect_input;
@@ -28,18 +32,20 @@ void crc8_init(crc8_t *p_crc8, crc8_conf_t const *p_crc8_conf) {
     }
 }
 
-uint8_t crc8_calc(crc8_t const *p_crc8, uint8_t const *p_data, size_t length) {
+uint8_t crc8_calc(crc8_t const *p_crc8, uint8_t const *data, size_t length) {
+    ASSERT(p_crc8);
+    ASSERT(data);
     uint8_t crc = p_crc8->initial_value;
     if (p_crc8->reflect_input) {
         while (length) {
-            crc = p_crc8->lookup_table[crc ^ bit_reverse(*p_data)];
-            p_data++;
+            crc = p_crc8->lookup_table[crc ^ bit_reverse(*data)];
+            data++;
             length--;
         }
     } else {
         while (length) {
-            crc = p_crc8->lookup_table[crc ^ *p_data];
-            p_data++;
+            crc = p_crc8->lookup_table[crc ^ *data];
+            data++;
             length--;
         }
     }
